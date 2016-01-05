e55f8a7411d9bdce519a2c5075c4cfb5247a4b90
vladhoncharenko <vladhoncharenko@gmail.com>
2016-01-05 11:24:59 +0200

    Added constructors for custom exception

--- src/main/java/app/model/NullTaskException.java
+++ src/main/java/app/model/NullTaskException.java
@@ -1,22 +1,34 @@
 
 package app.model;
+
 /**
  * Null Task Exception
- * 
+ *
  * @author Vlad Honcharenko
  * @version 1.0
  */
 
 public class NullTaskException extends Exception {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public NullTaskException() {
+    public NullTaskException() {
 
-	}
+    }
 
-	public NullTaskException(String message) {
-		super(message);
-	}
+    public NullTaskException(String message) {
+        super(message);
+    }
 
+    public NullTaskException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NullTaskException(Throwable cause) {
+        super(cause);
+    }
+
+    protected NullTaskException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
