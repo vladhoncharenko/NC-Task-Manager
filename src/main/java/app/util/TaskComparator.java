9298b4517f2fcd588600e0e3a9ad26ac6d1163f0
vladhoncharenko <vladhoncharenko@gmail.com>
2016-01-14 21:49:56 +0200

    Class TaskComparator started implements interface Serializable

--- src/main/java/app/util/TaskComparator.java
+++ src/main/java/app/util/TaskComparator.java
@@ -2,9 +2,10 @@ package app.util;
 
 import app.model.Task;
 
+import java.io.Serializable;
 import java.util.Comparator;
 
-public class TaskComparator implements Comparator<Task> {
+public class TaskComparator implements Comparator<Task>,Serializable {
 
     public int compare(Task o1, Task o2) {
         return o1.getExecutionDate().compareTo(o2.getExecutionDate());
